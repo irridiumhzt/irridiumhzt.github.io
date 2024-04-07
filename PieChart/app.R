a6e0300b52ef86e6e0c754e027fae36937f8974a
irridiumhzt <irridiumhzt@gmail.com>
2024-04-07 22:39:55 +0800

    change

--- PieChart/app.R
+++ PieChart/app.R
@@ -1,7 +1,17 @@
 library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+read_csv("LGBT_Survey_DailyLife.csv") 
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table"),
+  
   sidebarLayout(
     sidebarPanel(sliderInput("samplesize","Sample Size:",min = 100,max = 10000,value = 1000)),
     mainPanel(plotOutput("distPlot"))
@@ -11,10 +21,12 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   output$distPlot <- renderPlot({
-    hist(rnorm(input$samplesize),col='darkorchid',xlab="Sample",main="Standard Normally Distributed Sample")},
+    hist(rnorm(input$samplesize),col='darkorchid',xlab="",main="Standard Normally Distributed Sample")},
     height=300
   )
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
